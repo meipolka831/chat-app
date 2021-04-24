9b6d90e786e898ec129f296a97ee102cdd511221
meipolka831 <81464733+meipolka831@users.noreply.github.com>
2021-04-24 18:17:04 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
+  resources :message, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
